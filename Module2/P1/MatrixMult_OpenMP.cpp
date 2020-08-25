179b09597a2900a6328cf94b52d4db37c144aa46
ThamuZ808 <kane.lacey@hotmail.com>
2020-08-25 11:18:21 +1000

    Update MatrixMult_OpenMP.cpp

--- Module2/P1/MatrixMult_OpenMP.cpp
+++ Module2/P1/MatrixMult_OpenMP.cpp
@@ -4,20 +4,20 @@
 
 using namespace std;
 
-const int arrayX = 200;
-const int arrayY = 200;
+#define MAX_THREADS 3
+#define MAX 10
 
-int matrixA[arrayX][arrayY];
-int matrixB[arrayX][arrayY];
-int matrixC[arrayX][arrayY];
+int matrixA[MAX][MAX];
+int matrixB[MAX][MAX];
+int matrixC[MAX][MAX];
 
 int main()
 {
 	srand(time(NULL));
 
-	for (int i = 0; i < arrayX; ++i)
+	for (int i = 0; i < MAX; ++i)
 	{
-		for (int j = 0; j < arrayY; ++j)
+		for (int j = 0; j < MAX; ++j)
 		{
 			matrixA[i][j] = rand() % 9 + 1;
 			matrixB[i][j] = rand() % 9 + 1;
@@ -27,31 +27,32 @@ int main()
 
 	// This is the matrix multiplication to be timed
 	auto start = chrono::steady_clock::now();
-	
+
 	///////////
 	// OpenMP
 	///////////
-	#pragma omp parallel for private(i,j,k) shared(matrixA,matrixB,matrixC)	
-		for (int i = 0; i < arrayX; ++i)
-			for (int j = 0; j < arrayY; ++j)
-				for (int k = 0; k < arrayY; ++k)
-				{
-					matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
-				}
+	int i, j, k;
+	#pragma omp parallel for private(i,j,k) shared(matrixA,matrixB,matrixC)	num_threads(MAX_THREADS)
+	for (i = 0; i < MAX; ++i)
+		for (j = 0; j < MAX; ++j)
+			for (k = 0; k < MAX; ++k)
+			{
+				matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+			}
 
 	auto end = chrono::steady_clock::now();
 
 	cout << "Elapsed time in nanoseconds: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << "ns\n\n";
 
-	cout << "Output Matrix:\n";
-	for (int i = 0; i < arrayX; ++i)
+	/*cout << "Output Matrix:\n";
+	for (int i = 0; i < MAX; ++i)
 	{
-		for (int j = 0; j < arrayY; ++j)
+		for (int j = 0; j < MAX; ++j)
 		{
 			cout << matrixC[i][j] << " ";
 		}
 		cout << "\n";
-	}
+	}*/
 
 	cin;
 
